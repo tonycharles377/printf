eb5a7469698058901e9cc21a61aad09f90b70df3
Raji Bello Umar <umarrajibello@gmail.com>
2023-06-29 14:04:39 +0000

    Codded to update betty style

--- functions.c
+++ functions.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdlib.h>
 
 /**
  * print_char - prints character to stdout
@@ -56,7 +54,7 @@ int print_string(va_list list)
 int print_numd(va_list list)
 {
 	int count = 0, value = 0, num = va_list(list, int);
-	
+
 	if (num < 0)
 	{
 		value = _putchar('-');
@@ -67,7 +65,7 @@ int print_numd(va_list list)
 	{
 		print_numd(num / 10);
 	}
-	value =_ putchar('0' + (num % 10));
+	value = _ putchar('0' + (num % 10));
 	count = count + value;
 	return (count);
 }
